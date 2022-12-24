49e973e6e90ac5190e4f01c9b6cf4dfb34b7c424
gtchappy <2425172709@qq.com>
2022-12-24 13:04:35 +0800

    使用TSX代替template

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base:'/cookie/dist/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+      transformOn:true,
+      mergeProps:true
+    }),
+  ]
 })
