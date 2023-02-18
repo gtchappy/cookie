2c67e1dec610847cd279de5363f44b26f95fafb5
gtchappy <2425172709@qq.com>
2023-02-18 12:48:27 +0800

    重构:使用WelcomeLayout重构

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,22 +1,23 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
 import wallet from '../../assets/icons/wallet.svg'
+import { WelcomeLayout } from './WelcomeLayout'
+import s from './WelcomeLayout.module.scss'
 import { RouterLink } from 'vue-router';
 
 export const First = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    <img src={wallet}  alt={'钱包'}/>
-                    <h2> &nbsp; 会挣钱<br/>还会省钱</h2>
-                </div>
-                <div class={s.actions}>
-                    <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
-                    <RouterLink to="/welcome/2" >下一页</RouterLink>
-                    <RouterLink to="/start">跳过</RouterLink>
-                </div>
-            </div>
+            <WelcomeLayout>
+                {{
+                    icon: () => <img src={wallet} alt="钱包" />,
+                    title: () => <h2>会挣钱 <br /> 还会省钱</h2>,
+                    buttons: () => <>
+                        <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
+                        <RouterLink to="/welcome/3" >下一页</RouterLink>
+                        <RouterLink to="/start">跳过</RouterLink>
+                    </>
+                }}
+            </WelcomeLayout>
         )
     }
 })
--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -1,22 +1,23 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import { WelcomeLayout } from './WelcomeLayout';
+import s from './WelcomeLayout.module.scss'
 import checkout from '../../assets/icons/checkout.svg'
 import { RouterLink } from 'vue-router';
 
 export const Third = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    <img src={checkout} />
-                    <h2> &nbsp; 会挣钱<br/>还会省钱</h2>
-                </div>
-                <div class={s.actions}>
-                    <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
-                    <RouterLink to="/welcome/4" >下一页</RouterLink>
-                    <RouterLink to="/start">跳过</RouterLink>
-                </div>
-            </div>
+            <WelcomeLayout>
+                {{
+                    icon: () => <img src={checkout} alt="订单检查" />,
+                    title: () => <h2>会挣钱 <br /> 还会省钱</h2>,
+                    buttons: () => <>
+                        <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
+                        <RouterLink to="/welcome/4" >下一页</RouterLink>
+                        <RouterLink to="/start">跳过</RouterLink>
+                    </>
+                }}
+            </WelcomeLayout>
         )
     }
 })
